17f6988c7b9312d171a4b5c6de8e8995bb6893ef
Dulatto <dulat.omarov@gmail.com>
2022-04-28 22:21:28 -0400

    coderbyte initial

---
+++ coderbyte.js
@@ -0,0 +1,13 @@
+//First reverse
+
+//Solution 1
+
+function FirstReverse(str) {
+
+    // code goes here  
+    return str;
+
+}
+
+// keep this function call here 
+console.log(FirstReverse('Hello world'));
