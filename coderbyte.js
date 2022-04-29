8169fc617ac617d7243ab19817e4c6f5a9033d58
Dulatto <dulat.omarov@gmail.com>
2022-04-28 22:49:53 -0400

    coderbyte first reverse

--- coderbyte.js
+++ coderbyte.js
@@ -2,12 +2,49 @@
 
 //Solution 1
 
+// function FirstReverse(str) {
+//     return str.split('').reverse().join('');
+// }
+// // keep this function call here 
+// console.log(FirstReverse('Hello world'));
+
+
+// Solution 2
+
+// function FirstReverse(str) {
+//     let strReverse = '';
+//     for (let i = 0; i < str.length; i++) {
+//         strReverse = str[i] + strReverse;
+
+//     }
+//     return strReverse;
+// }
+// // keep this function call here 
+// console.log(FirstReverse('Hello world'));
+
+// Solution 3
+
+// function FirstReverse(str) {
+//     let strReverse = '';
+//     for (let char of str) {
+//         strReverse = char + strReverse;
+
+//     }
+//     return strReverse;
+// }
+// // keep this function call here 
+// console.log(FirstReverse('Hello world'));
+
+// Solution 4
+
 function FirstReverse(str) {
 
-    // code goes here  
-    return str;
+    const strToArr = str.split('');
 
+    const reversedStr = strToArr.reduce((acc, cv) => {
+        return cv + acc
+    })
+    return reversedStr
 }
-
 // keep this function call here 
 console.log(FirstReverse('Hello world'));
