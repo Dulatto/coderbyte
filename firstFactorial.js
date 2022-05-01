950277d46593334af55c6671eaa0a10951931568
Dulatto <dulat.omarov@gmail.com>
2022-04-30 21:09:40 -0400

    solution 2

--- firstFactorial.js
+++ firstFactorial.js
@@ -1,12 +1,49 @@
 // Solution 1
 
+// function FirstFactorial(num) {
+
+//     if (num < 0) {
+//         return;
+//     }
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//         result = result * i
+//     }
+//     return result;
+
+// }
+
+// // keep this function call here 
+// console.log(FirstFactorial(3));
+
+// Solution 2
+
+// function FirstFactorial(num) {
+
+//     if (num < 0) {
+//         return;
+//     }
+//     let result = 1;
+//     for (let i = num; i > 0; i--) {
+//         result = result * i
+//     }
+//     return result;
+
+// }
+
+// // keep this function call here 
+// console.log(FirstFactorial(3));
+
+
+// Solution 2
+
 function FirstFactorial(num) {
 
     if (num < 0) {
         return;
     }
     let result = 1;
-    for (let i = 1; i <= num; i++) {
+    for (let i = num; i > 0; i--) {
         result = result * i
     }
     return result;
