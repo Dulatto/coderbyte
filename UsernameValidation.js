79cbffbd01da8320690e65cb9fc54969b4a73e41
Dulatto <dulat.omarov@gmail.com>
2022-05-02 22:22:31 -0400

    solution 1

---
+++ UsernameValidation.js
@@ -0,0 +1,12 @@
+//Solution 1
+function CodelandUsernameValidation(str) {
+
+    // code goes here  
+    let valid_reg = /^[A-Za-z]\w+[A-Za-z0-9]$/;
+    let validLenght = (str) => str.length >= 4 && str.length <= 25;
+    return valid_reg.test(str) && validLenght(str);
+
+}
+
+// keep this function call here 
+console.log(CodelandUsernameValidation('u__hello_world123'));
