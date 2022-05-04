285e952b062f83da9e8b76bdae54f0c71d4ee4f1
Dulatto <dulat.omarov@gmail.com>
2022-05-03 21:25:25 -0400

    solution 2

--- UsernameValidation.js
+++ UsernameValidation.js
@@ -13,9 +13,19 @@
 
 //Solution 2
 
+// function CodelandUsernameValidation(str) {
+//     let userRegex = /^[a-z]{1}\w{2,23}[a-z|\d]{1}$/ig;
+//     return userRegex.test(str)
+// }
+
+// // keep this function call here 
+// console.log(CodelandUsernameValidation('u__hello_world123'));
+
+// Solution 3
+
 function CodelandUsernameValidation(str) {
-    let userRegex = /^[a-z]{1}\w{2,23}[a-z|\d]{1}$/ig;
-    return userRegex.test(str)
+    const regex = /^[a-zA-Z][a-zA-Z0-9_]*[^_]$/g
+    return str.length >= 4 && str.length <= 25 && regex.test(str)
 }
 
 // keep this function call here 
