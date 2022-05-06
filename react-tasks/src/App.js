3afe22da076f961d15a1d14fe307882ef9d86619
Dulatto <dulat.omarov@gmail.com>
2022-05-05 21:45:58 -0400

    react toogle

--- react-tasks/src/App.js
+++ react-tasks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Toggle from './Tasks/ReactButtonToggle';
 import Application from './Tasks/ReactPhoneBook';
 
 function App() {
   return (
     <div className="App">
-      <Application />
+      {/* <Application /> */}
+      <Toggle />
     </div>
   );
 }
