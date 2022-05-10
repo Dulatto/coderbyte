2cab327f76564dad444907eac1a43cc5773ded0d
Dulatto <dulat.omarov@gmail.com>
2022-05-09 22:51:38 -0400

    solution 1

--- LetterChanges.js
+++ LetterChanges.js
@@ -5,35 +5,62 @@
 //(a, e, i, o, u) and finally return this modified string.
 
 //Solution 1
+// function LetterChanges(str) {
+
+//     // A solution without regex or Unicode!
+
+//     // Write out alphabet and shifted alphabet with vowels capitalized
+//     const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+//     const NEW_ALPHABET = 'bcdEfghIjklmnOpqrstUvwxyzA';
+
+//     // Create storage array
+//     let answer = [];
+
+//     // Preprocess string to be all lowercase to match alphabet
+//     str = str.toLowerCase();
+
+//     // Loop through string
+//     for (let i = 0; i < str.length; i++) {
+
+//         // If alphabetic character, store shifted character from new alphabet
+//         if (ALPHABET.includes(str[i])) {
+//             answer.push(NEW_ALPHABET[ALPHABET.indexOf(str[i])]);
+
+//             // If non-alphabetic character, simply push that character to answer array
+//         } else {
+//             answer.push(str[i]);
+//         }
+//     }
+
+//     // Join the array into our final modified string!
+//     return answer.join('');
+// }
+
+//console.log(LetterChanges('Hello world'))
+
+//solution 2
+
 function LetterChanges(str) {
 
-    // A solution without regex or Unicode!
+    let array = str.split('');
+    array = array.map((letter) => {
+        var newLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
 
-    // Write out alphabet and shifted alphabet with vowels capitalized
-    const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
-    const NEW_ALPHABET = 'bcdEfghIjklmnOpqrstUvwxyzA';
-
-    // Create storage array
-    let answer = [];
-
-    // Preprocess string to be all lowercase to match alphabet
-    str = str.toLowerCase();
-
-    // Loop through string
-    for (let i = 0; i < str.length; i++) {
-
-        // If alphabetic character, store shifted character from new alphabet
-        if (ALPHABET.includes(str[i])) {
-            answer.push(NEW_ALPHABET[ALPHABET.indexOf(str[i])]);
-
-            // If non-alphabetic character, simply push that character to answer array
-        } else {
-            answer.push(str[i]);
+        if (!newLetter.match(/^[a-zA-Z]+$/)) {
+            return letter;
         }
-    }
+        else if (newLetter == 'a' || newLetter == 'e' || newLetter == 'i' || newLetter == 'o' || newLetter == 'u') {
+            return newLetter.toUpperCase();
+        }
+        else {
+            return newLetter.toLowerCase();
+        }
+    });
 
-    // Join the array into our final modified string!
-    return answer.join('');
+    return array.join('');
+
 }
 
+// keep this function call here 
 console.log(LetterChanges('Hello world'))
+
