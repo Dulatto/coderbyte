579bf4f830f9ef558c8e1c0e2e2aa06595a01c2c
Dulatto <dulat.omarov@gmail.com>
2022-05-10 22:28:36 -0400

    solution 2

--- LetterCapitalize.js
+++ LetterCapitalize.js
@@ -3,12 +3,21 @@
 //Words will be separated by only one space. 
 
 //Solution 1 
-function LetterCapitalize(str) {
+// function LetterCapitalize(str) {
 
-    let arr = str.split(" ");
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-    }
-    return arr.join(" ");
+//     let arr = str.split(" ");
+//     for (let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//     }
+//     return arr.join(" ");
+// }
+// console.log(LetterCapitalize('string to array of words by splitting on space'))
+
+//Solution 2
+
+function LetterCapitalize(str) {
+    return str.split(' ').map(item => item[0].toUpperCase().concat(item.slice(1))).join(' ');
 }
+
+// keep this function call here 
 console.log(LetterCapitalize('string to array of words by splitting on space'))
