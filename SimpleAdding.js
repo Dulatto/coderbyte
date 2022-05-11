db6d7a1cb1bbccd79f73b73b5906270bf09cab71
Dulatto <dulat.omarov@gmail.com>
2022-05-10 22:14:00 -0400

    solution 2

--- SimpleAdding.js
+++ SimpleAdding.js
@@ -2,14 +2,26 @@
 // For the test cases, the parameter num will be any number from 1 to 1000.
 
 //Solution 1
+// function SimpleAdding(num) {
+//     // Create storage for sum
+//     let sum = 0;
+//     // Add all values from 1 to num, inclusive
+//     for (let i = 1; i <= num; i++) {
+//         sum += i;
+//     }
+//     // Return total after adding
+//     return sum;
+// }
+// console.log(SimpleAdding(10))
+
+//Solution 2
 function SimpleAdding(num) {
-    // Create storage for sum
-    let sum = 0;
-    // Add all values from 1 to num, inclusive
-    for (let i = 1; i <= num; i++) {
-        sum += i;
-    }
-    // Return total after adding
-    return sum;
+    num = num * (num + 1) / 2;
+    // code goes here  
+    return num;
+
 }
+
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
 console.log(SimpleAdding(10))
