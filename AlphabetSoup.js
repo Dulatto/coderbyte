eeeb57a9614087f948bbfbaea196906ce2133dfc
Dulatto <dulat.omarov@gmail.com>
2022-05-12 21:07:02 -0400

    solution 1

---
+++ AlphabetSoup.js
@@ -0,0 +1,14 @@
+//Have the function AlphabetSoup(str) take the str string parameter being passed      *
+// and return the string with the letters in alphabetical order                        *
+// (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be       *
+// included in the string.
+
+//Solution 1
+
+function AlphabetSoup(str) {
+
+    return str.split("").sort().join("");
+
+}
+
+console.log(AlphabetSoup('world'))
