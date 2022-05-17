1620025efb7b60eedb674d122f0742549ec0d905
Dulatto <dulat.omarov@gmail.com>
2022-05-16 20:13:57 -0400

    solution 2

--- ArithGeo.js
+++ ArithGeo.js
@@ -33,33 +33,74 @@
 *                                                                                      *
 ***************************************************************************************/
 
+// function ArithGeo(arr) {
+
+//     var arithFlag = true, geoFlag = true;
+//     var diff = arr[1] - arr[0];
+
+//     for (var i = 2; i < arr.length; i++) {
+//         if ((arr[i] - arr[i - 1]) !== diff) {
+//             arithFlag = false;
+//         }
+//     }
+//     if (arithFlag) {
+//         return "Arithmetic";
+//     }
+//     else { // check for geometric pattern
+//         diff = arr[1] / arr[0];
+//         for (var i = 2; i < arr.length; i++) {
+//             if ((arr[i] / arr[i - 1]) !== diff) {
+//                 geoFlag = false;
+//             }
+//         }
+//         if (geoFlag) {
+//             return "Geometric";
+//         }
+//         else {
+//             return "-1";
+//         }
+//     }
+
+// }
+// console.log(ArithGeo([2, 6, 18, 54]))
+
+// Solution 2
+
 function ArithGeo(arr) {
 
-    var arithFlag = true, geoFlag = true;
-    var diff = arr[1] - arr[0];
+    // Store arithmetic difference and geometric ratio
+    let diff = arr[1] - arr[0];
+    let ratio = arr[1] / arr[0];
 
-    for (var i = 2; i < arr.length; i++) {
-        if ((arr[i] - arr[i - 1]) !== diff) {
-            arithFlag = false;
+    // Use counters to see if entire sequence is arithmetic or geometric
+    let arithCount = 0;
+    let geoCount = 0;
+
+    // Loop through sequence
+    for (let i = 0; i < arr.length - 1; i++) {
+
+        // If difference of terms matches arithmetic difference, add 1 to arithmetic counter
+        if (arr[i + 1] - arr[i] === diff) {
+            arithCount++;
         }
-    }
-    if (arithFlag) {
-        return "Arithmetic";
-    }
-    else { // check for geometric pattern
-        diff = arr[1] / arr[0];
-        for (var i = 2; i < arr.length; i++) {
-            if ((arr[i] / arr[i - 1]) !== diff) {
-                geoFlag = false;
-            }
-        }
-        if (geoFlag) {
-            return "Geometric";
-        }
-        else {
-            return "-1";
+
+        // If ratio of terms matches geometric ratio, add 1 to geometric counter
+        if (arr[i + 1] / arr[i] === ratio) {
+            geoCount++;
         }
     }
 
+    // If all terms share same difference, sequence is arithmetic
+    if (arithCount === arr.length - 1) {
+        return 'Arithmetic';
+    }
+
+    // If all terms share same ratio, sequence is geometric
+    if (geoCount === arr.length - 1) {
+        return 'Geometric';
+    }
+
+    // If both conditional checks fail, then sequence is neither arithmetic nor geometric!
+    return -1;
 }
 console.log(ArithGeo([2, 6, 18, 54]))
