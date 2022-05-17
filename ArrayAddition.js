6f254fbaf9dd046771f85710e2a5713af0a2b500
Dulatto <dulat.omarov@gmail.com>
2022-05-16 20:39:50 -0400

    solution 2

--- ArrayAddition.js
+++ ArrayAddition.js
@@ -25,33 +25,68 @@
 *                                                                                      *
 ***************************************************************************************/
 
+// function ArrayAdditionI(arr) {
+//     arr.sort(function (a, b) { return a - b });
+//     var maxNum = arr.pop();
+//     var tot = 0;
+
+//     for (var i = 0; i < arr.length; i++) {
+//         tot += arr[i];
+//         for (var j = 0; j < arr.length; j++) {
+//             if (i != j) {
+//                 tot += arr[j];
+//                 if (tot == maxNum) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         for (var k = 0; k < arr.length; k++) {
+//             if (i != k) {
+//                 tot -= arr[k];
+//                 if (tot == maxNum) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         tot = 0;
+//     }
+//     return false;
+// }
+
+// console.log(ArrayAdditionI([4, 6, 23, 10, 1, 3]))
+
+//Solution 2
+
 function ArrayAdditionI(arr) {
-    arr.sort(function (a, b) { return a - b });
-    var maxNum = arr.pop();
-    var tot = 0;
 
-    for (var i = 0; i < arr.length; i++) {
-        tot += arr[i];
-        for (var j = 0; j < arr.length; j++) {
-            if (i != j) {
-                tot += arr[j];
-                if (tot == maxNum) {
-                    return true;
-                }
-            }
+    // Calculate highest value by reducing array such that only the larger of each pair of values is returned
+    let highest = arr.reduce(function (p, v) { return v > p ? v : p; });
+
+    // Filter out highest value by keeping all values less than the highest value
+    let smallArr = arr.filter(function (val) { return val < highest; });
+
+    // Found this subsetsum function from lumberjack87 in Coderbyte forum
+    // Uses recursive template from Eloquent Javascript Ch.3 to search all possible combinations via branching
+    function subsetsum(target, array) {
+
+        // Base case when array is emptied
+        if (array.length === 0) {
+            return target === 0;
         }
 
-        for (var k = 0; k < arr.length; k++) {
-            if (i != k) {
-                tot -= arr[k];
-                if (tot == maxNum) {
-                    return true;
-                }
-            }
-        }
-        tot = 0;
+        // First term is stored
+        let n = array[0];
+
+        // The rest of the array is stored separately
+        array = array.slice(1);
+
+        // Call function with same target and sliced array OR subtract first value from target, then search sliced array 
+        return subsetsum(target, array) || subsetsum(target - n, array);
     }
-    return false;
+
+    // Recursive magic finds combination of values in smallArr that add to our highest value
+    return subsetsum(highest, smallArr);
 }
 
 console.log(ArrayAdditionI([4, 6, 23, 10, 1, 3]))
