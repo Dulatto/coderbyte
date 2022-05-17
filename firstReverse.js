156a47f5778d13b147a3a6b726f78cc268aea4bd
Dulatto <dulat.omarov@gmail.com>
2022-05-16 21:44:31 -0400

    description adding

--- firstReverse.js
+++ firstReverse.js
@@ -1,4 +1,19 @@
-//First reverse
+/***************************************************************************************
+*                                                                                      *
+*                  CODERBYTE BEGINNER CHALLENGE                                        *
+*                                                                                      *
+*  First Reverse                                                                       *
+*  Have the function FirstReverse() take the str parameter being passed and return     *
+*  the string in reversed order.                                                       *
+*                                                                                      *
+*  SOLUTION                                                                            *
+*  There is no reverse function for Strings in JavaScript BUT there is a reverse()     *
+*  function for arrays.  I will use this built-in function for my solution.            *
+*    1) Convert the string to an array using the split() function.                     *
+*    2) Use Array Reverse() function.                                                  *
+*    3) Convert array back to a string using join() function.                          *
+*                                                                                      *
+***************************************************************************************/
 
 //Solution 1
 
