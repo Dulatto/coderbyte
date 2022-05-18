b6b944e5a8356425220cf6df4211e52c4b1d8eca
Dulatto <dulat.omarov@gmail.com>
2022-05-17 21:21:26 -0400

    solution 2

--- LetterCount.js
+++ LetterCount.js
@@ -30,32 +30,84 @@
 *                                                                                      *
 ***************************************************************************************/
 
+// function LetterCountI(str) {
+//     var ctObj, tempWord, maxWord, maxCt = 1;
+//     var arr = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").split(" ");
+
+//     for (var i = 0; i < arr.length; i++) {
+//         tempWord = arr[i];
+//         ctObj = {}
+
+//         for (var j = 0; j < tempWord.length; j++) {
+//             ctObj[tempWord[j]] = ctObj[tempWord[j]] || 0;
+//             ctObj[tempWord[j]]++;
+//         }
+//         for (var key in ctObj) {
+//             if (ctObj.hasOwnProperty(key)) {
+//                 if (ctObj[key] > maxCt) {
+//                     maxCt = ctObj[key];
+//                     maxWord = tempWord;
+//                 }
+//             }
+//         }
+//     }
+
+//     if (maxCt === 1) {
+//         return -1;
+//     } else {
+//         return maxWord;
+//     }
+// }
+// console.log(LetterCountI('Today, is the greatest day ever!'))
+
+//Solution 2
 function LetterCountI(str) {
-    var ctObj, tempWord, maxWord, maxCt = 1;
-    var arr = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").split(" ");
 
-    for (var i = 0; i < arr.length; i++) {
-        tempWord = arr[i];
-        ctObj = {}
+    // Create object to store word objects, which will contain key-value pairs of letter: count and highest: #
+    let table = {};
 
-        for (var j = 0; j < tempWord.length; j++) {
-            ctObj[tempWord[j]] = ctObj[tempWord[j]] || 0;
-            ctObj[tempWord[j]]++;
-        }
-        for (var key in ctObj) {
-            if (ctObj.hasOwnProperty(key)) {
-                if (ctObj[key] > maxCt) {
-                    maxCt = ctObj[key];
-                    maxWord = tempWord;
+    // Break string into separate words
+    let arr = str.split(' ');
+
+    // Loop through words
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+
+        // Give each word a 'highest' property to tell us its highest letter count
+        table[word] = { highest: 1 };
+
+        // Loop through letters of each word
+        for (let j = 0; j < word.length; j++) {
+            let letter = word[j];
+
+            // If letter doesn't exist in word object, create it with a value of 1
+            if (table[word][letter] === undefined) {
+                table[word][letter] = 1;
+
+                // If letter exists, add 1 to its count and update highest if this letter has the new highest count in that word
+            } else {
+                table[word][letter]++;
+                if (table[word][letter] > table[word]['highest']) {
+                    table[word]['highest'] = table[word][letter];
                 }
             }
         }
     }
 
-    if (maxCt === 1) {
-        return -1;
-    } else {
-        return maxWord;
+    // Create storage for word with highest count
+    let answer = { word: null, highest: 1 };
+
+    // Loop through words in table to track highest count per word
+    for (let w in table) {
+
+        // If current word's highest count is greater than the overall highest, replace highest and store associated word
+        if (table[w]['highest'] > answer['highest']) {
+            answer['highest'] = table[w]['highest'];
+            answer['word'] = w;
+        }
     }
+
+    // If no letters repeat in any word, return -1. Otherwise, return word with the most repeated letters!
+    return (answer['highest'] > 1) ? answer['word'] : -1;
 }
 console.log(LetterCountI('Today, is the greatest day ever!'))
