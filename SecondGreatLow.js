d5e385b8355838c0c1b89edc779d96055d43505f
Dulatto <dulat.omarov@gmail.com>
2022-05-17 21:49:09 -0400

    solution 2

--- SecondGreatLow.js
+++ SecondGreatLow.js
@@ -20,24 +20,59 @@
 *    4) Return second and next to last values for answer                               *
 *                                                                                      *
 ***************************************************************************************/
+// function SecondGreatLow(arr) {
+
+//     if (arr.length === 2) {
+//         arr.sort(function (a, b) { return a - b });
+//         return arr[1] + " " + arr[0];
+//     }
+
+//     var uniqueArray = arr.filter(function (item, pos) {
+//         return arr.indexOf(item) == pos;
+//     });
+
+//     if (uniqueArray.length > 2) {
+//         uniqueArray.sort(function (a, b) { return a - b });
+//         return uniqueArray[1] + " " + uniqueArray[uniqueArray.length - 2];
+//     }
+//     else {
+//         return uniqueArray[1] + " " + uniqueArray[0];
+//     }
+
+// }
+// console.log(SecondGreatLow([7, 7, 12, 98, 106]))
+
+// Solution 2
 function SecondGreatLow(arr) {
 
+    // Sort array in ascending order
+    let sorted = arr.sort(function (a, b) { return a - b; });
+
+    // Edge case: if only two digits, we simply reverse them.
     if (arr.length === 2) {
-        arr.sort(function (a, b) { return a - b });
-        return arr[1] + " " + arr[0];
+        return sorted.reverse().join(' ');
     }
 
-    var uniqueArray = arr.filter(function (item, pos) {
-        return arr.indexOf(item) == pos;
-    });
+    // Store the largest and smallest values
+    let highest = arr[arr.length - 1];
+    let smallest = arr[0];
 
-    if (uniqueArray.length > 2) {
-        uniqueArray.sort(function (a, b) { return a - b });
-        return uniqueArray[1] + " " + uniqueArray[uniqueArray.length - 2];
-    }
-    else {
-        return uniqueArray[1] + " " + uniqueArray[0];
+    // Create solution storage
+    let answer = [];
+
+    // Loop through array
+    for (let i = 0; i < arr.length; i++) {
+
+        // If value is not equal to highest or lowest, store in answer array
+        if (arr[i] !== highest && arr[i] !== smallest) {
+            answer.push(arr[i]);
+        }
     }
 
+    // Sort new array without largest and smallest values in ascending order
+    let newSort = answer.sort(function (a, b) { return a - b; });
+
+    // Newly sorted array will have the second lowest and second greatest terms as first and last terms, respectively.
+    return answer[0] + " " + answer[answer.length - 1];
 }
 console.log(SecondGreatLow([7, 7, 12, 98, 106]))
