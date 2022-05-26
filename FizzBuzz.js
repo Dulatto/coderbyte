1ed4986e162f8daec48eb0cda1cfb7da41146ca0
Dulatto <dulat.omarov@gmail.com>
2022-05-25 20:57:25 -0400

    solution

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,9 +4,23 @@
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz"
 // instead of the number.
 
-for (let i = 1; i <= 100; ++i) {
-    if (i % 3 == 0) console.log('Fizz');
-    else if (i % 5 == 0) console.log('Buzz');
-    else if (i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz');
-    else console.log(i);
+
+//Solution 
+
+function fizzBuzz(start, end) {
+    for (let num = start; num <= end; num++) {
+        if (num % 5 === 0 && num % 3 === 0) {
+            console.log("FizzBuzz")
+        }
+        else if (num % 3 === 0) {
+            console.log("Fizz")
+        }
+        else if (num % 5 === 0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(num)
+        }
+    }
 }
+fizzBuzz(3, 45)
